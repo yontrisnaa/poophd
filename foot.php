15b10c9568e29b6e661279cf421adc5a154ae40c
Yon Trisna Kurniawan <105153046+yontrisnaa@users.noreply.github.com>
2025-02-25 14:34:17 +0700

    Create foot.php

---
+++ foot.php
@@ -0,0 +1,19 @@
+<footer class="bg-dark py-4">
+        <div class="container text-center">
+            <!-- Logo di tengah -->
+            <img src="https://i.doodcdn.co/img/logo-s.png" alt="JM" class="img-fluid" style="max-width: 150px;">
+            <p class="text-white mt-3">&copy; <?= date("Y"); ?> JM. All Rights Reserved.</p>
+        </div>
+    </footer>
+    <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4931602,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4931602&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
